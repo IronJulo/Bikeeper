4672a4455e15f7449d6e461d572505f2dbcefd57
jules <jules.brss@gmail.com>
2020-12-09 22:56:55 +0100

    ➕[ADD] Snippets read sms

---
+++ Arduino/Snippets/sms_reader/sms_reader.ino
@@ -0,0 +1,34 @@
+#include <SoftwareSerial.h>
+SoftwareSerial sim800l(2, 3);
+
+void setup()
+{
+  Serial.begin(9600);
+  sim800l.begin(9600);
+  Serial.println("Initializing..."); 
+  delay(1000);
+  sim800l.println("AT");
+  updateSerial();
+  sim800l.println("AT+CMGF=1");
+  updateSerial();
+  sim800l.println("AT+CNMI=1,2,0,0,0");
+  updateSerial();
+}
+
+void loop()
+{
+  updateSerial();
+}
+
+void updateSerial()
+{
+  delay(500);
+  while (Serial.available()) 
+  {
+    sim800l.write(Serial.read());
+  }
+  while(sim800l.available()) 
+  {
+    Serial.write(sim800l.read());
+  }
+}
--- Arduino/Snippets/sms_sender/sms_sender.ino
+++ Arduino/Snippets/sms_sender/sms_sender.ino
@@ -7,7 +7,7 @@
 
 SoftwareSerial sim800l(2, 3); // RX,TX for the Arduino and for the module it's TXD RXD, they should be inverted
 
-#define button1 7 //Button pin, on the other pin it's wired with GND
+#define button1 4 //Button pin, on the other pin it's wired with GND
 
 bool button_State; //Button state
 
@@ -36,26 +36,55 @@ void loop()
  }
  
   if (sim800l.available()){            //Displays on the serial monitor if there's a communication from the module
-    Serial.write(sim800l.read()); 
+    receivesms();
   }
 
   
 }
+
  
 void SendSMS()
 {
   Serial.println("Sending SMS...");               //Show this message on serial monitor
   sim800l.print("AT+CMGF=1\r");                   //Set the module to SMS mode
   delay(100);
-  sim800l.print("AT+CMGS=\"+33781546427\"\r");  //Your phone number don't forget to include your country code, example +212123456789"
-  delay(500);
-  sim800l.print("123456789123456789+132541654654646046546549806435465435132135468798646541351654698468684313213546876876843132165468798798764321321635465749876431321654685798713246546546798798765413215645787887546456123123546784568975461521245656478965454123456");       //This is the text to send to the phone number, don't make it too long or you have to modify the SoftwareSerial buffer
-  delay(500);
+  sim800l.print("AT+CMGS=\"+33769342048\"\r");  //Your phone number don't forget to include your country code, example +212123456789"
+  delay(100);
+  sim800l.print("vous etes un humain a 100%");       //This is the text to send to the phone number, don't make it too long or you have to modify the SoftwareSerial buffer
+  delay(100);
   sim800l.print((char)26);// (required according to the datasheet)
-  delay(500);
+  delay(100);
   sim800l.println();
   Serial.println("Text Sent.");
-  delay(500);
+  delay(100);
 
 }
+
+
+
+void GetLoc(){
+  Serial.println("je gette ta position mamen ...."); 
+  sim800l.print("AT+SAPBR=3,1,\"CONTYPE\",\"GPRS\"");
+  delay(100);
+  sim800l.print("AT+SAPBR=3,1,\"APN\",\"your apn here\"");
+  delay(100);
+  sim800l.print("AT+SAPBR=1,1");
+  delay(100);
+  sim800l.print("AT+CIPGSMLOC=1,1");
+  delay(100);
+  Serial.println("dataatad");
+
+}
+
+void receivesms(){
+  Serial.println("Receiving text message...");
+  sim800l.print("AT+CMGF=1r");   // Configure le mode SMS
+  // Affiche tous les messages
+  sim800l.print("AT+CMGL=\"ALL\"r");
+  delay(1000);
+  sim800l.println();
+}
+
+
+
  
