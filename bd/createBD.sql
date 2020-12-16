cc38a55ef08503f8b75ffedf8b52d1f4d6c69a9d
Andrew Mary Huet de Barochez <phoqueeberlue@gmail.com>
2020-12-16 14:06:00 +0100

    🔨Adding data bases files

---
+++ bd/createBD.sql
@@ -0,0 +1,69 @@
+CREATE DATABASE IF NOT EXISTS BIKEEPER DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE BIKEEPER;
+
+DROP TABLE CONTACT;
+DROP TABLE MESSAGE;
+DROP TABLE TICKET;
+DROP TABLE LOG;
+DROP TABLE DEVICE;
+DROP TABLE USER;
+
+CREATE TABLE CONTACT (
+  id_contact INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  num_contact VARCHAR(15),
+  firstname_contact VARCHAR(42),
+  lastname_contact VARCHAR(42),
+  profile_picture_contact VARCHAR(42),
+  num_device VARCHAR(15)
+);
+
+CREATE TABLE LOG (
+  id_log INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  content_log VARCHAR(160),
+  type_log VARCHAR(20),
+  datetime_log DATETIME,
+  exception_log VARCHAR(160),
+  num_device  VARCHAR(15)
+);
+
+CREATE TABLE DEVICE (
+  num_device VARCHAR(15),
+  name_device VARCHAR(42),
+  row_parameters_device VARCHAR(200),
+  username_user VARCHAR(42),
+  PRIMARY KEY (num_device)
+);
+
+CREATE TABLE USER (
+  username_user VARCHAR(42),
+  num_user VARCHAR(15),
+  firstname_user VARCHAR(42),
+  lastname_user VARCHAR(42),
+  email_user VARCHAR(42),
+  town_user VARCHAR(42),
+  postal_code_user INT(10),
+  street_user VARCHAR(42),
+  is_admin_user TINYINT,
+  PRIMARY KEY (username_user)
+);
+
+CREATE TABLE MESSAGE (
+  id_message INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  is_admin_message TINYINT,
+  datetime_message DATETIME,
+  title_message VARCHAR(100),
+  content_message VARCHAR(1000),
+  id_ticket INT
+);
+
+CREATE TABLE TICKET (
+  id_ticket INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  is_closed_ticket TINYINT,
+  username_user VARCHAR(42)
+);
+
+ALTER TABLE CONTACT ADD FOREIGN KEY (num_device) REFERENCES DEVICE (num_device);
+ALTER TABLE DEVICE ADD FOREIGN KEY (username_user) REFERENCES USER (username_user);
+ALTER TABLE MESSAGE ADD FOREIGN KEY (id_ticket) REFERENCES TICKET (id_ticket);
+ALTER TABLE TICKET ADD FOREIGN KEY (username_user) REFERENCES USER (username_user);
+ALTER TABLE LOG ADD FOREIGN KEY (num_device) REFERENCES DEVICE (num_device);
---
+++ bd/insertBD.sql
@@ -0,0 +1,34 @@
+-- Inserting a new user
+INSERT INTO USER(username_user, num_user, firstname_user, lastname_user, email_user, town_user, postal_code_user, street_user, is_admin_user)
+VALUES("PhoqueEberlue", "+33615498730", "Andrew", "Mary Huet de Barochez", "phoqueeberlue@gmail.com", "Chartres", "45100", "79 Rue de Georges lafont", 0);
+-- Inserting a new device
+INSERT INTO DEVICE(num_device, row_parameters_device, username_user)
+VALUES("+33625927620", "{'json example': 'deux'}", "PhoqueEberlue");
+-- Inserting a new log
+INSERT INTO LOG(content_log, type_log, datetime_log, exception_log, num_device)
+VALUES("content", "init", NOW(), "None", "+33625927620");
+-- Inserting a new contact
+INSERT INTO CONTACT(num_contact, firstname_contact, lastname_contact, profile_picture_contact, num_device)
+VALUES("+33798784565", "Dorian", "Hardy", " ", "+33625927620");
+INSERT INTO CONTACT(num_contact, firstname_contact, lastname_contact, profile_picture_contact, num_device)
+VALUES("+33798784569", "Kevin", "Talland", " ", "+33625927620");
+-- Inserting a new ticket
+INSERT INTO TICKET(is_closed_ticket, username_user)
+VALUES(0, "PhoqueEberlue");
+-- Insert a new message
+INSERT INTO MESSAGE(is_admin_message, datetime_message, title_message, content_message, id_ticket)
+VALUES(0, NOW(), "MON BIKEEPER NE FONCTIONNE PLUS CEST QUOI CETTE MERDE", "Bonjour mon bikeeper ne fonctionne plus, c'est très facheux nonobstant", 1);
+INSERT INTO MESSAGE(is_admin_message, datetime_message, title_message, content_message, id_ticket)
+VALUES(1, NOW(), "RE: MON BIKEEPER NE FONCTIONNE PLUS CEST QUOI CETTE MERDE", "Bonjour pourriez spécifiez d'avantage vos problèmes ? Cordialement MHDB.", 1);
+-- Select all devices of a given user
+SELECT num_device, row_parameters_device
+FROM DEVICE
+WHERE username_user = "PhoqueEberlue";
+-- Select every emergency contacts from a device
+SELECT id_contact, num_contact, firstname_contact, lastname_contact
+FROM CONTACT
+WHERE num_device = "+33625927620";
+-- Select all logs from a given device
+SELECT id_log, content_log, type_log, datetime_log, exception_log
+FROM LOG
+WHERE num_device = "+33625927620";
