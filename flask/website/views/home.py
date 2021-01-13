11243c07979377db632abb2e1721ab1eb18e903d
Nicolas <sunnico123456@gmail.com>
2021-01-13 10:11:59 +0100

    Template + views

--- flask/website/views/home.py
+++ flask/website/views/home.py
@@ -6,13 +6,33 @@ from flask import (
     url_for
 )
 from flask_mobility.decorators import mobile_template
+from flask_login import login_required
 
 mod = Blueprint('home', __name__)
 
 @mod.route('/home/', methods=['GET', 'POST'])
-@mobile_template('{mobile/Authentification/}home.html')
+# @mobile_template('{mobile/User/}dashboard.html')
 def home(template):
     return render_template(template)
+
+
+@mod.route('/mob/home/', methods=['GET', 'POST'])
+def mob_home():
+    return render_template("mobile/User/dashboard.html")
+
+@mod.route('/mob/support/', methods=['GET', 'POST'])
+def mob_support():
+    return render_template("mobile/User/support-chat.html")
+
+@mod.route('/mob/statistic/', methods=['GET', 'POST'])
+def mob_localisation():
+    return render_template("mobile/User/localisation.html")
+
+@mod.route('/mob/localisation/', methods=['GET', 'POST'])
+def mob_statistic():
+    return render_template("mobile/User/statistics.html")
+
+
 
 
 @mod.route('/home/user/<string:user_id>', methods=['GET'])
