ad660d38716217c11e8925acbcdb1077cfe96364
Kevin Talland <kevin.talland@etu.univ-orleans.fr>
2021-02-15 18:02:31 +0100

    ➕ [ADD] Search user from stats admin

--- flask/website/views/stats.py
+++ flask/website/views/stats.py
@@ -3,14 +3,32 @@ from flask import (
     render_template,
     session,
     redirect,
-    url_for
+    url_for,
+    request
 )
-
+from ..models import ORM
 from flask_mobility.decorators import mobile_template
 
 mod = Blueprint("stats", __name__)
 
-@mod.route('/statistics/', methods=['GET'])
+@mod.route('/statistics/', methods=['GET','POST'])
 @mobile_template('{mobile/User/}stats.html')
 def statistics(template):
-    return render_template(template)
+    search_user = request.form.get('search_user', default=None, type=str)
+    username=''
+    if search_user != None :
+        res = ORM.search_user(search_user)[0]
+        
+        return render_template(
+            template,
+            username = res.username_user,
+            phone = res.num_user,
+            street = res.street_user,
+            town = res.town_user,
+            postalcode = res.postal_code_user
+            )
+    else:
+        return render_template(
+            template,
+            username=username
+            )
