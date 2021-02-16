246786038892c3a7a8bcdb22a5764755b3035095
Andrew Mary Huet de Barochez <andrewmhdb@gmail.com>
2021-02-16 16:10:45 +0100

    🔥 Removing useless Jinja variables as this will be gathered with the API

--- flask/website/views/stats.py
+++ flask/website/views/stats.py
@@ -22,9 +22,6 @@ def statistics(template):
     search_user = request.form.get('search_user', default=None, type=str)
     username = ''
 
-    device_id = request.form.get('device-number')
-    ride_num = request.form.get('ride-number')
-
     if search_user is not None:  # ADMIN
         res = ORM.search_user(search_user)[0]
         return render_template(
@@ -33,17 +30,12 @@ def statistics(template):
             phone=res.num_user,
             street=res.street_user,
             town=res.town_user,
-            postalcode=res.postal_code_user,
-            rides=ORM.get_ride(res, device_id, ride_num),
-            devices=ORM.get_devices_by_username(res.username_user)
+            postalcode=res.postal_code_user
         )
     elif not current_user.is_admin_user:  # USER
         return render_template(
             template,
-            username=username,
-            rides=ORM.get_ride(current_user, device_id, ride_num),
-            devices=ORM.get_devices_by_username(current_user.username_user)
-
+            username=username
         )
     else:
         return render_template(
