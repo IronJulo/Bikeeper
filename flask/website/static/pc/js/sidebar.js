dc67c5013861a3df5f6d3c451da62a413dde0550
Kevin Talland <kevin.talland@etu.univ-orleans.fr>
2021-02-16 17:57:05 +0100

    [CSS] 🎨 Quick CSS fixes on home.css

--- flask/website/static/pc/js/sidebar.js
+++ flask/website/static/pc/js/sidebar.js
@@ -1,12 +1,11 @@
 
 let buttons_sidebar = document.querySelectorAll("#devices > form > button");
-let username = document.querySelector("#img-profile + span").textContent;
+let username_sidebar = document.querySelector("#img-profile + span").textContent;
 
 $.ajax({
-    url: "/api/current_device/"+username,
+    url: "/api/current_device/"+username_sidebar,
     success: function (result) {
         currentDevice = result.response;
-        console.log(currentDevice)
         for (let elem of buttons_sidebar){
             if (elem.value == currentDevice){
                 elem.style.color = "red";
