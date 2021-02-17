d092b64a48ca12fde4f9e77542df0f3f985ceb2f
Xavier <xavier.lemaire.28@gmail.com>
2021-02-17 14:59:26 +0100

    ✨ Adding timeline with informations to stats page

--- flask/website/static/pc/js/stats.js
+++ flask/website/static/pc/js/stats.js
@@ -113,6 +113,7 @@ const loadDateBikeepers = async () => { // updates the others fields to show the
         form_device_number.appendChild(node);
     }
     if (devices.length !== 0) {
+        await createTimelineElements();
         await loadRides();
     }
 }
@@ -135,6 +136,102 @@ const loadRides = async () => { // updates the field "rides" to show rides of a 
     }
     if (form_ride_number.value !== "") {
         await displayRideData();
+    }
+}
+
+const createTimelineElements = async() => {
+    let date = document.getElementById('ride-date').value;
+    let bikeeper = document.getElementById('device-number').value;
+    const response = await fetch('/api/bikeeper/get_logs_at_date/' + bikeeper + "/" + date);
+    timeline_logs = await response.json();
+    console.log(timeline_logs);
+    let div_timeline = document.getElementsByClassName('timeline')[0];
+    reset_timeline_by_class('timeline');
+    for (const log of timeline_logs.reverse()) {
+        let style = document.createElement("style");
+        div_timeline.appendChild(style);
+        let timeline_event = document.createElement("div");
+        timeline_event.classList.add("timeline__event",  "animated", "fadeInUp", "delay-3s", "timeline__event--type1");
+        let timeline_event_icon = document.createElement("div");
+        timeline_event_icon.classList.add("timeline__event__icon");
+        let icon = document.createElement("i");
+        icon.classList.add("fa", "fa-info-circle");
+        let timeline_date = document.createElement("div");
+        timeline_date.classList.add("timeline__event__date");
+        let timeline_content = document.createElement("div");
+        timeline_content.classList.add("timeline__event__content");
+        let timeline_title = document.createElement("div");
+        timeline_title.classList.add("timeline__event__title");
+        let timeline_description = document.createElement("div");
+        timeline_description.classList.add("timeline__event__description");
+        let timeline_description_text = document.createElement("p");
+        let date_content = document.createTextNode(log.datetime_log);
+        div_timeline.appendChild(timeline_event);
+        timeline_event.appendChild(timeline_event_icon);
+        timeline_event_icon.appendChild(icon);
+        timeline_event_icon.appendChild(timeline_date);
+        timeline_event.appendChild(timeline_content);
+        timeline_content.appendChild(timeline_title);
+        timeline_content.appendChild(timeline_description);
+        timeline_description.appendChild(timeline_description_text);
+        timeline_date.appendChild(date_content);
+        if (log.type_log !== "W"){
+            let t = "";
+            if (log.type_log == "+"){
+                if (log.content_log.type == "C"){
+                    t = "Un trajet a commencé.";
+                }
+                else if (log.content_log.type == "D"){
+                    t = "Un trajet s'est terminé.";
+                }
+            }
+
+            let title_content = document.createTextNode("Information")
+            timeline_title.appendChild(title_content);
+            let style_content = document.createTextNode(":root {\n" +
+                "                                    --timeline-main-color: #7BC6E1;\n" +
+                "                                }");
+            style.appendChild(style_content);
+            let timeline_description_text_content = document.createTextNode(t)
+            timeline_description_text.appendChild(timeline_description_text_content);
+        }
+        else{
+            let t = "";
+            let text_content = "";
+            if (log.content_log.type == "V"){
+                t = "Vibration détectée";
+                text_content = "Une vibration a été détectée sur votre Bikeeper aux coordonnées suivantes :\n" +
+                "Longitude : " + log.content_log.longitude + "\n" +
+                "Latitude : " + log.content_log.latitude + "\n";
+            }
+            else if (log.content_log.type == "G"){
+                t = "Signal GPS";
+                text_content = "Votre Bikeeper a commencé à bouger depuis les coordonnées suivantes :\n" +
+                "Longitude : " + log.content_log.longitude + "\n" +
+                "Latitude : " + log.content_log.latitude + "\n";
+            }
+            else if (log.content_log.type == "F"){
+                t = "Chute du véhicule";
+                text_content = "Votre Bikeeper est tombé aux coordonnées suivantes :\n" +
+                "Longitude : " + log.content_log.longitude + "\n" +
+                "Latitude : " + log.content_log.latitude + "\n";
+            }
+            let title_content = document.createTextNode("Alerte - " + t)
+            let style_content = document.createTextNode(":root {\n" +
+                "                                    --timeline-main-color: #DC1D21;\n" +
+                "                                }");
+            style.appendChild(style_content);
+            timeline_title.appendChild(title_content);
+            let timeline_description_text_content = document.createTextNode(text_content);
+            timeline_description_text.appendChild(timeline_description_text_content);
+        }
+    }
+}
+
+function reset_timeline_by_class(classe){
+    let timeline = document.getElementsByClassName(classe)[0];
+    while (timeline.firstChild){
+        timeline.removeChild(timeline.lastChild);
     }
 }
 
