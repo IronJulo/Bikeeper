e80cfa00bf69bc30c197195e7efa0fece67a3e59
Andrew Mary Huet de Barochez <andrewmhdb@gmail.com>
2021-02-17 10:42:57 +0100

    ♻️ Improving path simulation

--- Arduino/server/sendtrajet.py
+++ Arduino/server/sendtrajet.py
@@ -2,13 +2,15 @@ import requests
 import random
 import json
 
+sender = "0769342048"
+
 for i in range(10):
     url = "http://127.0.0.1:5000/api/sms/add/"
     headers = {
         'Content-Type': 'application/json'
     }
 
-    payload = {"header": {"key": "[bk]", "schema": "+", "sender": "0769342048"},
+    payload = {"header": {"key": "[bk]", "schema": "+", "sender": sender},
                 "data": {"type": "C"}}
     response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
 
@@ -16,13 +18,14 @@ for i in range(10):
     lat = 47.90176300000000
     speed = 50
     angle = 0
-    sign = 1 if random.random() < 0.5 else -1
     for i in range(25):
+        sign = 1 if random.random() < 0.5 else -1
         lon += random.random() * sign * 0.01
+        sign = 1 if random.random() < 0.5 else -1
         lat += random.random() * sign * 0.01
         speed += random.randint(-9, 9)
         angle += random.randint(-1, 1)
-        payload = {"header": {"key": "[bk]", "schema": "@", "sender": "0769342048"},
+        payload = {"header": {"key": "[bk]", "schema": "@", "sender": sender},
                 "data": {"longitude": lon, "latitude": lat, "charge": "f", "level": [50, 42], "speed": speed, "angle": angle}}
 
 
@@ -30,6 +33,6 @@ for i in range(10):
 
         print(response.text)
 
-    payload = {"header": {"key": "[bk]", "schema": "+", "sender": "0769342048"},
+    payload = {"header": {"key": "[bk]", "schema": "+", "sender": sender},
                 "data": {"type": "D"}}
     response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
